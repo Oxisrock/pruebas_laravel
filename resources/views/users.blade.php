db43fb4c8e440e58eab072a02d235a31c85b8544
Francisco Aular <franciscoaular18@gmail.com>
2018-05-24 14:05:27 -0400

    agregando vista didinamica

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,23 +1,22 @@
 <div class="">
 <table style="width:50%">
   <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Age</th>
+    <th>Nombre</th>
+    <th>Apellido</th>
+    <th>Cedula</th>
+    <th>Email</th>
+    <th>Educacion</th>
+    <th>Fecha de nacimiento</th>
   </tr>
+    @foreach($users as $user)
   <tr>
-    {{$users}}
-    <td style="
-    text-align: center;">Jill</td>
-    <td style="
-    text-align: center;">Smith</td>
-    <td style="
-    text-align: center;">50</td>
+    <td style="text-align: center;">{{ $user->firt_name }}</td>
+    <td style="text-align: center;">{{ $user->last_name }}</td>
+    <td style=" text-align: center;">{{ $user->cedula }}</td>
+    <td style=" text-align: center;">{{ $user->email }}</td>
+    <td style=" text-align: center;">{{ $user->education }}</td>
+    <td style=" text-align: center;">{{ $user->birthday }}</td>
   </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
+    @endforeach
 </table>
 </div>
